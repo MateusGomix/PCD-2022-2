1f042917e87b3c9c57a3aeac2b00082123635b64
Luizpson <luizfelipe_cda@hotmail.com>
2022-10-01 22:50:37 -0300

    Padronizado mais ou menos

--- T1/final/pthreadsJogoDaVida.c
+++ T1/final/pthreadsJogoDaVida.c
@@ -23,14 +23,15 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <omp.h>
 
 /*##############< Size Parameters >#############*/
-#define NUM 2048
+#define N 2048
 #define THREADS_NUMBER 4
 #define PLAY_TIMES 2000
 
 /*##############< Mode controller >#############*/
-#define HIGH_LIFE 0
+#define HIGH_LIFE 1
 
 /* Grid global pointers */
 int **newgrid;
@@ -55,40 +56,32 @@ void *parsing(void *args);
 void init(thread_t *parsersInit);
 int count(void);
 void printGrid(void);
+void SaidaFinal(double start,int soma_final);
 
 /*##############< Main Program >#############*/
 int main(){
-    struct timeval inicio, final2;
-
     int **grid1;
     int **grid2;
-    int tmili;
 
-    grid1 = (int**) malloc(NUM * sizeof(int*));
-    grid2 = (int**) malloc(NUM * sizeof(int*));
+    grid1 = (int**) malloc(N * sizeof(int*));
+    grid2 = (int**) malloc(N * sizeof(int*));
 
-    for(int i = 0; i < NUM; i++){
-        grid1[i] = (int*) malloc(NUM * sizeof(int));
-        grid2[i] = (int*) malloc(NUM * sizeof(int));
+    for(int i = 0; i < N; i++){
+        grid1[i] = (int*) malloc(N * sizeof(int));
+        grid2[i] = (int*) malloc(N * sizeof(int));
     }
 
     newgrid = (int **) grid1;
     grid    = (int **) grid2;
 
     thread_t sweepers[THREADS_NUMBER];
-    //gettimeofday(&inicio, NULL);
+
     init(sweepers);
-    //gettimeofday(&final2, NULL);
-
-    //tmili = (int) (1000 * (final2.tv_sec - inicio.tv_sec) + (final2.tv_usec - inicio.tv_usec) / 1000);
-
-    //printf("tempo decorrido da init: %d milisegundos\n", tmili);
-
-    //printGrid();
 
     pthread_barrier_init(&barrier, NULL, THREADS_NUMBER);
 
-    gettimeofday(&inicio, NULL);
+    double start;
+    start = omp_get_wtime();
 
     for(int j = 0; j < THREADS_NUMBER; j++){
         pthread_create(&sweepers[j].t, NULL, parsing, &sweepers[j]);
@@ -98,24 +91,26 @@ int main(){
         pthread_join(sweepers[j].t, NULL);
     }
 
-    gettimeofday(&final2, NULL);
+    SaidaFinal(start,count());
 
     //printGrid();
-
-    tmili = (int) (1000 * (final2.tv_sec - inicio.tv_sec) + (final2.tv_usec - inicio.tv_usec) / 1000);
-
-    if(HIGH_LIFE) printf("< HIGHLIFE >\n");
-    else printf("< JOGO DA VIDA >\n");
-
-    printf("Dimensões: %dx%d \nThreads: %d \nGerações: %d \n------------------------- \n", NUM, NUM, THREADS_NUMBER, PLAY_TIMES);
-    
-    printf("Tempo paralelo: %.3fs\n", tmili/1000.00);
-
-    printf("Somatório: %d\n\n", count());
 
     return 0;
 }
 
+
+void SaidaFinal(double start,int soma_final){
+    double end = omp_get_wtime();
+
+    if(HIGH_LIFE) printf("< HIGHLIFE >\n");
+    else printf("< JOGO DA VIDA >\n");
+
+    printf("Dimensões: %dx%d \nThreads: %d \nGerações: %d \n------------------------- \n", N, N, THREADS_NUMBER, PLAY_TIMES);
+
+    printf("Tempo paralelo: %.3fs\n", end - start);
+
+    printf("Somatório: %d\n\n", soma_final);
+}
 /*##############< Custom Methods >#############*/
 
 /* Return the number of alive neighbors
@@ -127,11 +122,11 @@ int numero_vizinhos(int** matriz, int x, int y)
     int yc = y - 1, yb = y + 1;
     int xe = x - 1, xd = x + 1;
 
-    if(xe < 0) xe = NUM-1;
-    else if(xd >= NUM) xd = 0;
+    if(xe < 0) xe = N-1;
+    else if(xd >= N) xd = 0;
 
-    if(yc < 0) yc = NUM-1;
-    else if(yb >= NUM) yb = 0;
+    if(yc < 0) yc = N-1;
+    else if(yb >= N) yb = 0;
 
     total =   matriz[x][yc]
             + matriz[x][yb]
@@ -166,8 +161,8 @@ void *parsing(void *args){
     thread_t *aux = (thread_t *) args;
 
     for(int k = 0; k < PLAY_TIMES; k++){
-        for(int i = aux->threadNumber; i < NUM; i += THREADS_NUMBER){
-            for(int j = 0; j < NUM; j++){
+        for(int i = aux->threadNumber; i < N; i += THREADS_NUMBER){
+            for(int j = 0; j < N; j++){
 
                 int neighborsNumber = numero_vizinhos(grid, i, j);
 
@@ -200,8 +195,8 @@ void init(thread_t *parsersInit){
         parsersInit[i].threadNumber = i;
     }
 
-    for(int i = 0; i < NUM; i++){
-        for(int j = 0; j < NUM; j++){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
 
             grid[i][j] = 0;
             newgrid[i][j] = 0;
@@ -232,8 +227,8 @@ void init(thread_t *parsersInit){
 int count(void){
     int total = 0;
 
-    for(int i = 0; i < NUM; i++){
-        for(int j = 0; j < NUM; j++){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
             total += grid[i][j];
         }
     }
@@ -245,8 +240,8 @@ int count(void){
  */
 void printGrid(void){
 
-    for(int i = 0; i < NUM; i++){
-        for(int j = 0; j < NUM; j++){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
             printf("%d ", grid[i][j]);
         }
         printf("\n");
